6fb9602d882f66001c25a10826fe41b926adf829
EduardoNdL <eduardo.lima@sptech.school>
2025-09-02 12:12:40 -0300

    RA: 04251108 - Eduardo Nunes de Lima

--- src/main/java/school/sptech/Gincana.java
+++ src/main/java/school/sptech/Gincana.java
@@ -1,5 +1,83 @@
 package school.sptech;
 
 public class Gincana {
+    private String nome;
+    private Double premio;
+    private Integer qtdInscrito;
+    private Boolean ativa;
+
+    public Gincana(String nome, Double premio) {
+        setNome(nome);
+        this.premio = premio;
+        this.qtdInscrito = 0;
+        this.ativa = true;
+    }
+
+    public void inscrever(Integer qtd) {
+        if (qtd != null && qtd > 0 && ativa) {
+            qtdInscrito = qtd;
+        }
+    }
+
+    public Integer remover(Integer qtd) {
+        if (qtd != null && qtd > 0 && qtd <= qtdInscrito && ativa) {
+            qtdInscrito -= qtd;
+            return qtd;
+        } else {
+            return null;
+        }
+    }
+
+    public Integer desativar() {
+        if (!ativa) {
+            return null;
+        } else {
+            ativa = false;
+            Integer qtdAntesDesativar = qtdInscrito;
+            qtdInscrito = 0;
+            return qtdAntesDesativar;
+        }
+    }
+
+    public void transferir(Gincana destino, Integer qtdATransferir) {
+        if (qtdATransferir != null && qtdATransferir > 0 &&
+                destino != null && destino.ativa &&
+                qtdATransferir <= qtdInscrito
+                && this.ativa) {
+            destino.qtdInscrito += qtdATransferir;
+            this.qtdInscrito -= qtdATransferir;
+        }
+
+    }
+
+    public Boolean reajustarPremio(Double reajuste){
+        if(reajuste != null && reajuste > 0) {
+            premio+= (premio * reajuste);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Double getPremio() {
+        return premio;
+    }
+
+    public Integer getQtdInscrito() {
+        return qtdInscrito;
+    }
+
+    public Boolean getAtiva() {
+        return ativa;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 
 }
--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -4,5 +4,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Boa sorte!");
         System.out.println("Você pode testar manualmente a classe Gincana aqui se quiser....");
+        Gincana aa = new Gincana("gincana1", 25.00 );
+        aa.inscrever(10);
+        System.out.println(aa.remover(2));
+
+        Gincana bb = new Gincana("gincana2", 30.00 );
+        bb.inscrever(10);
+
+
+        aa.transferir(bb, 4);
+        System.out.println(aa.getQtdInscrito());
+        System.out.println(bb.getQtdInscrito());
+
+        System.out.println(bb.reajustarPremio(0.5));
     }
 }
